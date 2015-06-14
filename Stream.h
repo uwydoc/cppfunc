1c896e4fb54a2cb2ad8485e178fa0979c0e1966a
uwydoc <uwydoc@gmail.com>
2015-06-15 01:34:59 +0800

    - Add 'step' argument for `intsFrom` and `range`, fix non-stop bug in range - Implement 'fmap' to add 'Functor' support - Implement 'mbind' and 'mreturn' to add 'Monad' support

--- Stream.h
+++ Stream.h
@@ -9,6 +9,7 @@
 #include <functional>
 #include <iostream>
 #include <memory>
+#include <type_traits>
 
 #include "Susp.h"
 
@@ -47,11 +48,11 @@ template<typename T>
 using Cell = typename Stream<T>::Cell;
 
 /// intsFrom
-Stream<int> intsFrom(int t)
+Stream<int> intsFrom(int t, int step=1)
 {
-    return Stream<int>([t]()
+    return Stream<int>([t, step]()
     {
-        return Cell<int>(t, intsFrom(t+1));
+        return Cell<int>(t, intsFrom(t+step));
     });
 }
 /// take
@@ -66,15 +67,15 @@ Stream<T> take(Stream<T> s, int n)
 }
 /// range
 template<typename T>
-Stream<T> range(T begin, T end);
+Stream<T> range(T begin, T end, T step=T(1));
 template<>
-Stream<int> range<int>(int begin, int end)
+Stream<int> range<int>(int begin, int end, int step)
 {
-    if (begin == end)
+    if (begin >= end)
         return Stream<int>();
     else
-        return Stream<int>([begin, end]() {
-            return Cell<int>(begin, range<int>(begin+1, end));
+        return Stream<int>([begin, end, step]() {
+            return Cell<int>(begin, range<int>(begin+step, end));
         });
 }
 /// output
@@ -85,6 +86,67 @@ std::ostream& operator<<(std::ostream& os, const Stream<T>& s)
         return os;
     os << s.get() << ' ';
     return (os << s.next());
+}
+/// forEach
+template<typename T, typename F>
+void forEach(Stream<T> s, F f)
+{
+    while (!s.isEmpty()) {
+        f(s.get());
+        s = s.next();
+    }
+}
+/// concat
+template<typename T>
+Stream<T> concat(Stream<T> s1, Stream<T> s2)
+{
+    if (s1.isEmpty())
+        return s2;
+    return Stream<T>([s1, s2]() {
+        return Cell<T>(s1.get(), concat(s1.next(), s2));
+    });
+}
+/// @group Functor fmap
+template<typename T, typename F, typename R = typename std::result_of<F(T)>::type>
+Stream<R> fmap(Stream<T> s, F f)
+{
+    if (s.isEmpty())
+        return Stream<R>();
+
+    return Stream<R>([s, f]()
+    {
+        return Cell<R>(f(s.get()), fmap(s.next(), f));
+    });
+}
+/// @group Monad mjoin
+template<typename T>
+Stream<T> mjoin(Stream<Stream<T> > ss)
+{
+    if (ss.isEmpty())
+        return Stream<T>();
+    if (ss.get().isEmpty())
+        return mjoin(ss.next());
+    return Stream<T>([ss]()
+    {
+        Stream<T> hd = ss.get();
+        return Cell<T>(hd.get(),
+            mjoin(Stream<Stream<T> >([hd, ss]() {
+                    return Cell<Stream<T> >(hd.next(), ss.next());
+            }))
+        );
+    });
+}
+/// @group Monad mbind
+template<typename T, typename F, typename StreamR = typename std::result_of<F(T)>::type>
+StreamR mbind(Stream<T> s, F f)
+{
+    return mjoin(fmap(s, f));
+}
+/// @group Monad mreturn
+template<typename T>
+Stream<T> mreturn(T t)
+{
+    return Stream<T>([t]() { return Cell<T>(t); });
 }
 
 #endif // CPPFUNC_STREAM_H_
--- StreamTest.cpp
+++ StreamTest.cpp
@@ -6,6 +6,22 @@
 
 int main()
 {
+    std::cout << "--- intsFrom" << std::endl;
     std::cout << take(intsFrom(1), 10) << std::endl;
+    std::cout << "--- range" << std::endl;
     std::cout << take(range(10, 20), 5) << std::endl;
+    std::cout << "--- forEach" << std::endl;
+    forEach(range(30, 33), [](int t) {
+        std::cout << t << std::endl;
+    });
+    std::cout << "--- concat" << std::endl;
+    std::cout << concat(range(1, 5), range(5, 10)) << std::endl;
+    // Functor
+    std::cout << "--- fmap" << std::endl;
+    std::cout << fmap(range(10, 15), [](int t) { return t+1; }) << std::endl;
+    // Monad
+    std::cout << "--- mreturn" << std::endl;
+    std::cout << mreturn(3) << std::endl;
+    std::cout << "--- mbind" << std::endl;
+    std::cout << mbind(range(1, 4), [](int x) { return range(1, x+1); }) << std::endl;
 }
